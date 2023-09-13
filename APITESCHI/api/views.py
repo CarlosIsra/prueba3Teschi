232142af0043313bb1694c585f0da0288cbe5e37
CarlosIsra <2019452016@teschi.edu.mx>
2023-09-13 15:08:35 -0600

    v2.2

--- APITESCHI/api/views.py
+++ APITESCHI/api/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
-
+from rest_framework.views import APIView
 # Create your views here.
+
+
+class home(APIView):
+        template_name = "index.html"
+        def get(self, request):
+            return render(request, self.template_name)
+    
