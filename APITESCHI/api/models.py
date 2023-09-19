50d01154fc11d7969d41d346c97c3e215c05db56
CarlosIsra <2019452016@teschi.edu.mx>
2023-09-19 09:42:20 -0600

    2.6

--- APITESCHI/api/models.py
+++ APITESCHI/api/models.py
@@ -4,16 +4,5 @@ from django.db import models
 
 
 #creacion de la BD. 
-
-class alumno(models.Model):
-    idAlumno = models.IntegerField(primary_key=True,db_column='idAlumno')
-    
-    
-    class Producto(models.Model):
-        nombre = models.CharField(max_length=50)
-        precio = models.IntegerField()
-        descripcion = models.TextField()d
-        nuevo = models.ForeignKey(Marca, on_delete=models.PROTECT)
-        fecha_fabricacion = models.DateField()
         
         
